fbdcabbd6348e532c6495fe0916137433b59aa93
Prashandip Lunghimba <prashandip2@gmail.com>
2020-09-10 10:53:39 +0545

    Updates

---
+++ precache-manifest.589b9aef6cb98be8963d1d185a6aa3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dca2d07a450d56969988ef6e1a8836",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "fcd2955a98b64fdde143",
+    "url": "/recipeapp/static/css/main.911a2580.chunk.css"
+  },
+  {
+    "revision": "7e68185431ebe3504f25",
+    "url": "/recipeapp/static/js/2.224214a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeapp/static/js/2.224214a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd2955a98b64fdde143",
+    "url": "/recipeapp/static/js/main.2be3645d.chunk.js"
+  },
+  {
+    "revision": "d3b4d9703729fc3efee5",
+    "url": "/recipeapp/static/js/runtime-main.792d144b.js"
+  }
+]);
